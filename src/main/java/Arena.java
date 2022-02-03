a83d2af7c4caa6c11dc37cf3296fa9c82241f525
FrankyArnaud <tchomfambiarnaud@gmail.com>
2022-02-03 14:40:26 +0100

    implementation de la fonction fight

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -1,6 +1,11 @@
 public class Arena {
 
     public Robot fight(Robot robot1, Robot robot2) {
-        return robot2;
+        while(!robot1.isDead() && !robot2.isDead()) {
+            double random = Math.random()*10;
+            if(random > 5) robot1.fire(robot2);
+            else robot2.fire(robot1);
+        }
+        return robot1.isDead() ? robot2: robot1;
     }
 }
